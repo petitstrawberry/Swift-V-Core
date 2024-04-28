56f3a49606fadbcd22156945af76dc5479202fa5
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-04-28 23:17:43 +0900

    remove: H-extension exceptions interrupts

--- Sources/SwiftVCore/Exception.swift
+++ Sources/SwiftVCore/Exception.swift
@@ -13,17 +13,11 @@ extension Exception {
     static let loadAccessFault = Exception(exceptionCode: 5, priority: 1)
     static let storeAMOAddressMisaligned = Exception(exceptionCode: 6, priority: 1)
     static let storeAMOAccessFault = Exception(exceptionCode: 7, priority: 1)
-    static let environmentCallFromUModeOrVUMode = Exception(exceptionCode: 8, priority: 1)
+    static let environmentCallFromUMode = Exception(exceptionCode: 8, priority: 1)
     static let environmentCallFromSMode = Exception(exceptionCode: 9, priority: 1)
-    static let environmentCallFromHMode = Exception(exceptionCode: 10, priority: 1)
-    static let environmentCallFromMMode = Exception(exceptionCode: 11, priority: 1)
     static let instructionPageFault = Exception(exceptionCode: 12, priority: 1)
     static let loadPageFault = Exception(exceptionCode: 13, priority: 1)
     static let storeAMOPageFault = Exception(exceptionCode: 15, priority: 1)
-    static let instructionGuestPageFault = Exception(exceptionCode: 20, priority: 1)
-    static let loadGuestPageFault = Exception(exceptionCode: 21, priority: 1)
-    static let virtualInstruction = Exception(exceptionCode: 22, priority: 1)
-    static let storeAMOGuestPageFault = Exception(exceptionCode: 23, priority: 1)
 }
 
 
--- Sources/SwiftVCore/Interrupt.swift
+++ Sources/SwiftVCore/Interrupt.swift
@@ -6,13 +6,9 @@ public struct Interrupt: Trap {
 
 extension Interrupt {
     static let supervisorSoftwareInterrupt = Interrupt(exceptionCode: 1, priority: 1)
-    static let virtualSupervisorSoftwareInterrupt = Interrupt(exceptionCode: 2, priority: 1)
     static let machineSoftwareInterrupt = Interrupt(exceptionCode: 3, priority: 1)
     static let supervisorTimerInterrupt = Interrupt(exceptionCode: 5, priority: 1)
-    static let virtualSupervisorTimerInterrupt = Interrupt(exceptionCode: 6, priority: 1)
     static let machineTimerInterrupt = Interrupt(exceptionCode: 7, priority: 1)
     static let supervisorExternalInterrupt = Interrupt(exceptionCode: 9, priority: 1)
-    static let virtualSupervisorExternalInterrupt = Interrupt(exceptionCode: 10, priority: 1)
     static let machineExternalInterrupt = Interrupt(exceptionCode: 11, priority: 1)
-    static let supervisorGuestExternalInterrupt = Interrupt(exceptionCode: 12, priority: 1)
 }
