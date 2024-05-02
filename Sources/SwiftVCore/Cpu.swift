0e08648f476c911352d4e6c0a04bf1d72f02843e
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-02 15:35:37 +0900

    impl:  Csr, CsrProtocol

--- Sources/SwiftVCore/Cpu.swift
+++ Sources/SwiftVCore/Cpu.swift
@@ -1,4 +1,5 @@
 public class Cpu {
+
     public enum PriviligedMode: UInt8 {
         case machine = 0b11
         case supervisor = 0b01
@@ -8,12 +9,16 @@ public class Cpu {
     var pc: UInt32 = 0
     var xregs: Xregisters = Xregisters()
     var fregs: Fregisters = Fregisters()
+    var csrBank: CsrBank
+
     var mode: PriviligedMode = .machine
     var memory: Memory
     var instructionTable = InstructionTable()
+
     public init(memory: Memory, instructionSets: [InstructionSet]) {
         self.memory = memory
         instructionTable.load(instructionSets: instructionSets)
+        csrBank = CsrBank(instructionSets: instructionSets)
     }
 
     public func run() {
@@ -23,24 +28,26 @@ public class Cpu {
         while (!interrupt && !exception) {
             // Fetch
             let inst: UInt32 = memory.read32(pc)
-            // Decode
-            let opcode: Int = Int(inst & 0x07f)
 
+            // Decode
+            let opcode: Int = Int(inst & 0b111_1111)
+            print("PC: 0x\(String(pc, radix: 16)) inst: 0b\(String(inst, radix: 2)) Opcode: 0b\(String(opcode, radix: 2))")
             // Execute
             if let type = instructionTable.typeTable[Int(opcode)] {
                 switch type {
                 case .R:
                     let funct3 = Int((inst >> 12) & 0x07)
                     let funct7 = Int((inst >> 25) & 0x7f)
-                    instructionTable.rTable[opcode][funct7][funct3]?.execute(cpu: self, inst: inst)
+                    try instructionTable.rTable[opcode][funct7][funct3]?.execute(cpu: self, inst: inst)
                 case .I, .S, .B:
                     let funct3 = Int((inst >> 12) & 0x07)
-                    instructionTable.isbTable[opcode][funct3]?.execute(cpu: self, inst: inst)
+                    try instructionTable.isbTable[opcode][funct3]?.execute(cpu: self, inst: inst)
                 case .U, .J:
-                    instructionTable.ujTable[opcode]?.execute(cpu: self, inst: inst)
+                    try instructionTable.ujTable[opcode]?.execute(cpu: self, inst: inst)
                 }
             } else {
                 print("Unknown opcode: 0b\(String(opcode, radix: 2))")
+
                 break
             }
         }
@@ -49,23 +56,8 @@ public class Cpu {
         for i in 0..<32 {
             let reg = xregs.read(UInt32(i))
             // print unsigned, signed, binary
-            print("x\(i): \(reg), \(Int32(bitPattern: reg))")
+            print("\(Xregisters.RegName(rawValue: i)!): \(reg), \(Int32(bitPattern: reg))", terminator: ", ")
         }
-
-        // // print memory
-
-        // for i in stride(from: 0x10000,
-        //                 to: 0x1000f,
-        //                 by: 4) {
-        //     print("0x\(String(i, radix: 16)):", terminator: " ")
-
-        //     for j in 0..<3 {
-        //         print("0x\(String(memory.mem[i + j], radix: 16))", terminator: " ")
-        //     }
-        //     print("0x\(String(memory.mem[i + 3], radix: 16))")
-        // }
-
-
-
+        print()
     }
 }
