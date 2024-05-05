e4d803e059bc1d8896091491b2a9560adcbdbc92
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-05 12:59:31 +0900

    add: default impl

--- Sources/SwiftVCore/InstructionSets/InstructionSet.swift
+++ Sources/SwiftVCore/InstructionSets/InstructionSet.swift
@@ -2,3 +2,8 @@ public protocol InstructionSet {
     var instructions: [Instruction] { get }
     var csrs: [Csr] { get }
 }
+
+extension InstructionSet {
+    var instructions: [Instruction] { return [] }
+    var csrs: [Csr] { return [] }
+}
