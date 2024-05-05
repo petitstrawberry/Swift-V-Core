c6f2e647214ee07c24f813311e03bbd540da6ec1
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-05 23:32:47 +0900

    change: extend Trap

--- Sources/SwiftVCore/Trap.swift
+++ Sources/SwiftVCore/Trap.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Trap: Error {
-    case exception(Exception)
-    case interrupt(Interrupt)
+    case exception(_ trap: Exception, tval: UInt32 = 0)
+    case interrupt(_ trap: Interrupt, tval: UInt32 = 0)
 }
