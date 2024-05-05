2f21a27d86c97bce8272fce8f8a64f3c78f821bf
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-05 14:48:43 +0900

    change: Trap: Error

--- Sources/SwiftVCore/Exception.swift
+++ Sources/SwiftVCore/Exception.swift
@@ -1,21 +1,15 @@
-public struct Exception: Trap {
-    public let exceptionCode: UInt32
-    public let interrupt: Bool = false
-    public let priority: UInt8
-}
-
-extension Exception {
-    static let instructionAddressMisaligned = Exception(exceptionCode: 0, priority: 0)
-    static let instructionAccessFault = Exception(exceptionCode: 1, priority: 0)
-    static let illegalInstruction = Exception(exceptionCode: 2, priority: 0)
-    static let breakpoint = Exception(exceptionCode: 3, priority: 0)
-    static let loadAddressMisaligned = Exception(exceptionCode: 4, priority: 0)
-    static let loadAccessFault = Exception(exceptionCode: 5, priority: 0)
-    static let storeAMOAddressMisaligned = Exception(exceptionCode: 6, priority: 0)
-    static let storeAMOAccessFault = Exception(exceptionCode: 7, priority: 0)
-    static let environmentCallFromUMode = Exception(exceptionCode: 8, priority: 0)
-    static let environmentCallFromSMode = Exception(exceptionCode: 9, priority: 0)
-    static let instructionPageFault = Exception(exceptionCode: 12, priority: 0)
-    static let loadPageFault = Exception(exceptionCode: 13, priority: 0)
-    static let storeAMOPageFault = Exception(exceptionCode: 15, priority: 0)
+public enum Exception: UInt32 {
+    case instructionAddressMisaligned = 0
+    case instructionAccessFault = 1
+    case illegalInstruction = 2
+    case breakpoint = 3
+    case loadAddressMisaligned = 4
+    case loadAccessFault = 5
+    case storeAMOAddressMisaligned = 6
+    case storeAMOAccessFault = 7
+    case environmentCallFromUMode = 8
+    case environmentCallFromSMode = 9
+    case instructionPageFault = 12
+    case loadPageFault = 13
+    case storeAMOPageFault = 15
 }
--- Sources/SwiftVCore/Interrupt.swift
+++ Sources/SwiftVCore/Interrupt.swift
@@ -1,14 +1,8 @@
-public struct Interrupt: Trap {
-    public let exceptionCode: UInt32
-    public let interrupt: Bool = true
-    public let priority: UInt8
-}
-
-extension Interrupt {
-    static let supervisorSoftwareInterrupt = Interrupt(exceptionCode: 1, priority: 1)
-    static let machineSoftwareInterrupt = Interrupt(exceptionCode: 3, priority: 1)
-    static let supervisorTimerInterrupt = Interrupt(exceptionCode: 5, priority: 1)
-    static let machineTimerInterrupt = Interrupt(exceptionCode: 7, priority: 1)
-    static let supervisorExternalInterrupt = Interrupt(exceptionCode: 9, priority: 1)
-    static let machineExternalInterrupt = Interrupt(exceptionCode: 11, priority: 1)
+public enum Interrupt: UInt32 {
+    case supervisorSoftwareInterrupt = 1
+    case machineSoftwareInterrupt = 3
+    case supervisorTimerInterrupt = 5
+    case machineTimerInterrupt = 7
+    case supervisorExternalInterrupt = 9
+    case machineExternalInterrupt = 11
 }
