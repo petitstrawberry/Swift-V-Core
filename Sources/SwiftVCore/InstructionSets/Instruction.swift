6a6c3af23cd1eed2a55453df53e61263aef5670a
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-25 10:40:50 +0900

    add: check cpu mode when exec priv inst

--- Sources/SwiftVCore/InstructionSets/Instruction.swift
+++ Sources/SwiftVCore/InstructionSets/Instruction.swift
@@ -13,15 +13,19 @@ public struct Instruction: Identifiable {
     public let id = UUID()
     public let name: String
     public let type: InstructionType
+    public let mode: Cpu.PriviligedMode
     public let opcode: UInt8
     public let funct3: UInt8?
     public let funct7: UInt8?
 
     public let closure: (Cpu, UInt32) throws -> Void
 
-    public init(name: String, type: InstructionType, opcode: UInt8, funct3: UInt8? = nil, funct7: UInt8? = nil, closure: @escaping (Cpu, UInt32) throws -> Void) {
+    public init(name: String, type: InstructionType, mode: Cpu.PriviligedMode = .user,
+                opcode: UInt8, funct3: UInt8? = nil, funct7: UInt8? = nil,
+                closure: @escaping (Cpu, UInt32) throws -> Void) {
         self.name = name
         self.type = type
+        self.mode = mode
         self.opcode = opcode
         self.funct3 = funct3
         self.funct7 = funct7
@@ -29,6 +33,12 @@ public struct Instruction: Identifiable {
     }
 
     func execute(cpu: Cpu, inst: UInt32) throws {
+        // check if the instruction is a privileged instruction
+        if opcode == 0b1110011 {
+            if cpu.mode.rawValue < mode.rawValue {
+                throw Trap.exception(.illegalInstruction)
+            }
+        }
         try closure(cpu, inst)
     }
 }
