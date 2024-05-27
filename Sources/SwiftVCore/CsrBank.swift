45dee3b6ee462ace53b54feee2a0826c7b83f9c5
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-05-27 14:28:22 +0900

    change: CSRの実装を変更したい #15 init csrs with dummy at init()

--- Sources/SwiftVCore/CsrBank.swift
+++ Sources/SwiftVCore/CsrBank.swift
@@ -1,13 +1,19 @@
 public struct CsrBank {
     static let kCSRCount = 0x1000
 
-    var csrs: [Csr?] = Array(repeating: nil, count: kCSRCount)
+    var csrs: [Csr]
+
+    public init() {
+        // Initialize all CSRs to a dummy CSR
+        let csr = Csr(name: "Dummy CSR", addr: UInt32(0), value: 0)
+        self.csrs = Array(repeating: csr, count: CsrBank.kCSRCount)
+    }
 
     public mutating func load(instructionSets: [InstructionSet]) {
         for instructionSet in instructionSets {
             for csr in instructionSet.csrs {
                 self.csrs[Int(csr.addr)] = csr
-                print("Loaded CSR: \(csr.name)")
+                print("Loaded CSR at 0x\(String(csr.addr, radix: 16)): \(csr.name)")
             }
         }
     }
