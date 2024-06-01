dda9502497b2f671aa536edc97ad2666f8733cea
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-01 13:39:10 +0900

    impl: default func for Device

--- Sources/SwiftVCore/Devices/Device.swift
+++ Sources/SwiftVCore/Devices/Device.swift
@@ -9,3 +9,36 @@ public protocol Device {
     func read32(addr: UInt64) -> UInt32
     func write32(addr: UInt64, data: UInt32)
 }
+
+extension Device {
+    public func read8(addr: UInt64) -> UInt8 {
+        fatalError("read8 not implemented")
+    }
+
+    public func write8(addr: UInt64, data: UInt8) {
+        fatalError("write8 not implemented")
+    }
+
+    public func read16(addr: UInt64) -> UInt16 {
+        return UInt16(read8(addr: addr)) | UInt16(read8(addr: addr + 1)) << 8
+    }
+
+    public func write16(addr: UInt64, data: UInt16) {
+        write8(addr: addr, data: UInt8(data & 0xff))
+        write8(addr: addr + 1, data: UInt8((data >> 8) & 0xff))
+    }
+
+    public func read32(addr: UInt64) -> UInt32 {
+        return UInt32(read8(addr: addr))
+            | UInt32(read8(addr: addr + 1)) << 8
+            | UInt32(read8(addr: addr + 2)) << 16
+            | UInt32(read8(addr: addr + 3)) << 24
+    }
+
+    public func write32(addr: UInt64, data: UInt32) {
+        write8(addr: addr, data: UInt8(data & 0xff))
+        write8(addr: addr + 1, data: UInt8((data >> 8) & 0xff))
+        write8(addr: addr + 2, data: UInt8((data >> 16) & 0xff))
+        write8(addr: addr + 3, data: UInt8((data >> 24) & 0xff))
+    }
+}
