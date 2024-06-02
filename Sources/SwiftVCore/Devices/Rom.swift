21ad22e41e049f692b8083f2f4da3444e3b32734
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-02 13:13:41 +0900

    add: device tree

--- Sources/SwiftVCore/Devices/Rom.swift
+++ Sources/SwiftVCore/Devices/Rom.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public let kRomSize: UInt32 = 0xf000 // 60KB
 
 public class Rom: Device {
@@ -10,7 +12,8 @@ public class Rom: Device {
         rom = Array(repeating: UInt8(), count: Int(size))
         let resetProgram: [UInt32] = [
             0x800002b7, // lui      t0,0x80000
-            0xf1402573, // csrr     a0,mhartid
+            0xf1402573, // csrr    a0,mhartid
+            0x000025b7, // lui      a1,0x2
             0x00028067  // jr      t0 # 80000000 <_start+0x80000000>
         ]
 
@@ -21,10 +24,22 @@ public class Rom: Device {
             rom[index * 4 + 2] = UInt8((value >> 16) & 0xff)
             rom[index * 4 + 3] = UInt8((value >> 24) & 0xff)
         }
+
+        // load dtb from file
+        let dtbPath = "Sources/SwiftVCore/Resources/swiftv.dtb"
+        let dtbData = try! Data(contentsOf: URL(fileURLWithPath: dtbPath))
+        let dtbSize = dtbData.count
+        let dtbAddr: UInt64 = 0x2000
+        for i in 0..<dtbSize {
+            rom[Int(dtbAddr - startAddr) + i] = dtbData[i]
+        }
     }
 
     public func read8(addr: UInt64) -> UInt8 {
-        return rom[Int(addr - startAddr)]
+        let ret =  rom[Int(addr - startAddr)]
+        print("addr: 0x\(String(addr, radix: 16))")
+        print("Read8: 0x\(String(ret, radix: 16))")
+        return ret
     }
 
     public func read16(addr: UInt64) -> UInt16 {
@@ -35,10 +50,12 @@ public class Rom: Device {
 
     public func read32(addr: UInt64) -> UInt32 {
         let intAddr = Int(addr - startAddr)
-        return UInt32(rom[intAddr]) as UInt32
+        let ret =  UInt32(rom[intAddr]) as UInt32
             | UInt32(rom[intAddr + 1]) << 8 as UInt32
             | UInt32(rom[intAddr + 2]) << 16 as UInt32
             | UInt32(rom[intAddr + 3]) << 24 as UInt32
+        print("Read32: 0x\(String(ret, radix: 16))")
+        return ret
     }
 
     public func write8(addr: UInt64, data: UInt8) {
