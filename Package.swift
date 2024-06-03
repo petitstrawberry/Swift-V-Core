ba788bc37e974cfb0731b1cb223ac634afadb3d4
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-03 17:24:33 +0900

    fix: Package.swift

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
             dependencies: ["SwiftVCore"],
             resources: [
                 .copy("Resources")
-            ],
-        ),
+            ]
+        )
     ]
 )
