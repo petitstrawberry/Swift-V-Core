2f1e70267da5162d1d34d15b62ec81ef32d7760d
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-03 16:46:36 +0900

    add: dummy SFENCE instructions

--- Sources/SwiftVCore/InstructionSets/SupervisorLevelISA/SupervisorLevelISA.swift
+++ Sources/SwiftVCore/InstructionSets/SupervisorLevelISA/SupervisorLevelISA.swift
@@ -1,6 +1,23 @@
 struct SupervisorLevelISA: InstructionSet {
     let isa = 1 << 18
-    var instructions: [Instruction] = []
+    var instructions: [Instruction] = [
+        // SFENCE.VMA
+        Instruction(name: "SFENCE.VMA", type: .R, mode: .supervisor,
+                        opcode: 0b1110011, funct3: 0b000, funct7: 0b0001001) { cpu, _ in
+            cpu.pc &+= 4
+        },
+        // SINVAL.VMA
+        Instruction(name: "SINVAL.VMA", type: .R, mode: .supervisor,
+                        opcode: 0b1110011, funct3: 0b000, funct7: 0b0001011) { cpu, _ in
+            cpu.pc &+= 4
+        },
+        // SFENCE.W.INVAL / SFENCE.INVAL.IR
+        Instruction(name: "SFENCE.W.INVAL/SFENCE.INVAL.IR", type: .R, mode: .supervisor,
+                        opcode: 0b1110011, funct3: 0b000, funct7: 0b0001100) { cpu, _ in
+            cpu.pc &+= 4
+        }
+    ]
+
     var csrs: [Csr] = [
         Satp()
     ]
