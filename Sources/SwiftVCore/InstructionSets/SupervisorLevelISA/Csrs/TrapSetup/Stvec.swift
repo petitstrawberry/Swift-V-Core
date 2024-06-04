993ffb1bc514bc5ed6cb8b58b4664c33bcae99d5
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-04 14:25:04 +0900

    impl: S-mode Trap

--- Sources/SwiftVCore/InstructionSets/SupervisorLevelISA/Csrs/TrapSetup/Stvec.swift
+++ Sources/SwiftVCore/InstructionSets/SupervisorLevelISA/Csrs/TrapSetup/Stvec.swift
@@ -2,12 +2,12 @@ extension CsrBank.RegAddr {
     static let stvec: UInt32 = 0x105
 }
 
-class Stvec: Csr {
+class Stvec: Mtvec {
     public required init(name: String, addr: UInt32, value: UInt32 = 0) {
         super.init(name: name, addr: addr, value: value)
     }
 
-    init() {
+    override init() {
         super.init(name: "stvec", addr: 0x105, value: 0)
     }
 }
