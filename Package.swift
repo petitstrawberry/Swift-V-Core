891eb77ee64581cc4583532b0b474cad0cce87cd
PetitStrawberry <deloreantaro@yahoo.co.jp>
2024-06-11 10:52:59 +0900

    refactor: optimize option to safe

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
             swiftSettings: [
                 // optimize for testing
                 .unsafeFlags([
-                    "-Ounchecked"
+                    "-O"
                 ])
             ]
         ),
